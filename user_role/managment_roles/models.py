162051c21cf5c0a3a7ba9b5a89287d9d9b28ad08
wl_demon_lw <curkanpavel97@gmail.com>
2024-02-03 14:41:22 +0200

    create data base and models in it

---
+++ user_role/managment_roles/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Role(models.Model):
+    name = models.CharField(max_length=25)
+
+    def str(self):
+        return self.name
+
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    role = models.ForeignKey(Role, on_delete=models.DO_NOTHING)
+
+    
